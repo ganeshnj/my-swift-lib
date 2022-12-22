cc2ca27b5b782e7b25fff821f318efa188bdb8e0
Ganesh Jangir <jangirg@amazon.com>
2022-12-22 13:37:21 -0800

    add Greeter target

---
+++ Sources/Greeter/Greeter.swift
@@ -0,0 +1,8 @@
+/// A simple Swift library.
+public struct Greeter {
+    let text = "Hello, World!"
+    /// Prints "Hello, world!" to the console.
+    public func sayHello() {
+        print(text)
+    }
+}
